a36f0f327580d087003a6425b9e5eee136e49a1a
pooneh <p.moghadda30@gmail.com>
2024-03-07 13:27:00 +0100

    Docker in dev mode set up

---
+++ store/src/main/java/com/bookz/store/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.bookz.store.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+
+    @PostMapping()
+    public String createUser(@RequestBody String user){
+        return "User created mama mia cassela!dbhvnl" +
+                " " +
+                "horrray!" +
+                "joanan";
+    }
+
+}
